2279a1513a3f0ea0248085fb2d39f489b7d93794
KnotekOndrej <ondrejknotek@s.zochova.sk>
2023-12-04 11:05:23 +0100

    new: code

---
+++ backend/src/native-test/java/quarkus/react/tech/tests/ExampleResourceIT.java
@@ -0,0 +1,8 @@
+package quarkus.react.tech.tests;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+class ExampleResourceIT extends ExampleResourceTest {
+    // Execute the same tests but in packaged mode.
+}
